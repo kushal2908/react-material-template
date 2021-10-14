5450dba25548559b14ff843250785f193586e70a
sarder safa bin salam <sarder.safa@datasoft-bd.com>
2021-10-14 17:41:09 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Grid, Paper } from "@mui/material";
 import Layout from "./Components/Layout/Layout";
 function App() {
   return (
     <div>
-      <Layout />
+      <Layout>
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={8}>
+            <Paper>xs=6 md=8</Paper>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <Paper>xs=6 md=4</Paper>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <Paper>xs=6 md=4</Paper>
+          </Grid>
+          <Grid item xs={6} md={8}>
+            <Paper>xs=6 md=8</Paper>
+          </Grid>
+        </Grid>
+      </Layout>
     </div>
   );
 }
--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -7,19 +7,17 @@ import CssBaseline from "@mui/material/CssBaseline";
 import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
-
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { Button, Menu, MenuItem } from "@mui/material";
+import { Avatar, Menu, MenuItem, Tooltip } from "@mui/material";
 import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Logout, PersonAdd, Settings } from "@mui/icons-material";
 
 const drawerWidth = 240;
 
@@ -69,17 +67,16 @@ const DrawerHeader = styled("div")(({ theme }) => ({
 export default function PersistentDrawerLeft({ children }) {
   const theme = useTheme();
   const [open, setOpen] = React.useState(true);
-  const [open2, setOpen2] = React.useState(false);
-
+  // const [open2, setOpen2] = React.useState(false);
   const [anchorEl, setAnchorEl] = React.useState(null);
 
+  const open2 = Boolean(anchorEl);
   const handleClick = (event) => {
-    // setAnchorEl(event.currentTarget);
-    setOpen2(!open2);
+    setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     // setAnchorEl(null);
-    setOpen2(false);
+    setAnchorEl(null);
   };
 
   const handleDrawerOpen = () => {
@@ -94,47 +91,85 @@ export default function PersistentDrawerLeft({ children }) {
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <div className="topbar">
-            <div>
-              {open === true ? (
-                <IconButton aria-label="left" onClick={handleDrawerClose}>
-                  <MenuOpenIcon style={{ color: "white" }} />
-                </IconButton>
-              ) : (
-                <IconButton aria-label="right" onClick={handleDrawerOpen}>
-                  <MenuIcon style={{ color: "white" }} />
-                </IconButton>
-              )}
-            </div>
-
-            <div>
-              <IconButton
-                id="basic-button"
-                aria-label="left"
-                onClick={handleClick}
-                aria-expanded={open2 ? "true" : undefined}
-              >
-                <AccountCircleIcon style={{ color: "white" }} />
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          {/* Sidenav Toggle */}
+          {open === true ? (
+            <IconButton aria-label="left" onClick={handleDrawerClose}>
+              <MenuOpenIcon style={{ color: "white" }} />
+            </IconButton>
+          ) : (
+            <IconButton aria-label="right" onClick={handleDrawerOpen}>
+              <MenuIcon style={{ color: "white" }} />
+            </IconButton>
+          )}
+          {/* User Account */}
+          <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+            <Tooltip title="Account settings">
+              <IconButton onClick={handleClick} size="small" sx={{ ml: 2 }}>
+                <Avatar src="/static/images/avatar/1.jpg" sx={{ width: 32, height: 32 }} />
               </IconButton>
-              <Menu
-                id="basic-menu"
-                open={open2}
-                onClick={handleClose}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                MenuListProps={{
-                  "aria-labelledby": "basic-button",
-                }}
-              >
-                <MenuItem>Profile</MenuItem>
-                <MenuItem>My account</MenuItem>
-                <MenuItem>Logout</MenuItem>
-              </Menu>
-            </div>
-          </div>
+            </Tooltip>
+          </Box>
+          <Menu
+            anchorEl={anchorEl}
+            open={open2}
+            onClose={handleClose}
+            onClick={handleClose}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                overflow: "visible",
+                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                mt: 1.5,
+                "& .MuiAvatar-root": {
+                  width: 32,
+                  height: 32,
+                  ml: -0.5,
+                  mr: 1,
+                },
+                "&:before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 14,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "background.paper",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
+            <MenuItem>
+              <Avatar /> Profile
+            </MenuItem>
+            <MenuItem>
+              <Avatar /> My account
+            </MenuItem>
+            <Divider />
+            <MenuItem>
+              <ListItemIcon>
+                <PersonAdd fontSize="small" />
+              </ListItemIcon>
+              Add another account
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <Settings fontSize="small" />
+              </ListItemIcon>
+              Settings
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
       <Drawer
@@ -177,7 +212,7 @@ export default function PersistentDrawerLeft({ children }) {
           ))}
         </List>
       </Drawer>
-      <Main open={open}>
+      <Main open={open} sx={{ background: "aliceblue" }}>
         <DrawerHeader />
         <div className="mainBody">
           {children}
